205883d71e09d83a302c810c1229fc5460495500
kianoushmahboob <kianoushmhaboob@gmail.com>
2020-07-09 09:34:09 +0430

    اضفاه کردن جدول پروژه ها

---
+++ client/pages/crm/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <Table />
+</template>
+
+<script>
+import Table from "../../components/crm/table";
+export default {
+  layout: "auth",
+  components: {
+    Table
+  }
+};
+</script>
